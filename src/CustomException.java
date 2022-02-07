557b6e589e13f96bd9f9421956da5901719b655d
Bako Laszlo <79322093+bakolaszlo@users.noreply.github.com>
2022-02-07 09:49:29 +0200

    Added exception example.

---
+++ src/CustomException.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class CustomException extends Throwable {
+    private String cause;
+    private LocalDateTime time;
+
+    public CustomException(String cause, LocalDateTime time) {
+        this.cause = cause;
+        this.time = time;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+
+public class Main {
+
+    public static void main(String[] args) throws CustomException {
+        System.out.println("Enter something here:");
+        try{
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            String s = bufferRead.readLine();
+            System.out.println("\nYou entered : ");
+            System.out.println(s);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new CustomException("Custom exception", LocalDateTime.now());
+        }
+    }
+}
